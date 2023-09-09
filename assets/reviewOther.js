5f55e476e66830f4142a136bf075b083b67fa41f
ravi-marmeto <ravi@marmeto.com>
2023-09-09 22:18:24 +0530

    initial commit

---
+++ assets/reviewOther.js
@@ -0,0 +1,2 @@
+(function(e){"use strict";T4SThemeSP.reviewOther=function(){}})(jQuery_T4NT),jQuery_T4NT(document).ready(function(e){jQuery_T4NT("body").on("reloadReview.t4s",T4SThemeSP.reviewOther)});
+//# sourceMappingURL=/s/files/1/0661/7423/t/254/assets/reviewOther.js.map?v=1682157414
